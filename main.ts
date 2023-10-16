3a67792ce47d2e5b7de952447a70c010048df4e0
Jedsadaporn pannok <41636063+tikpoptv@users.noreply.github.com>
2023-10-17 01:52:17 +0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -50,50 +50,13 @@ function BL_RedayShoot () {
         iBIT.Turn(ibitTurn.Left, 50)
     }
 }
-function objectfollowing () {
-    if (y > 155 && (x >= 120 && x <= 200)) {
-        iBIT.MotorStop()
-        basic.showLeds(`
-            # # # # #
-            # . . . #
-            # . . . #
-            # . . . #
-            # # # # #
-            `)
-        ready_crashOBJ()
-    } else if (y < 155 && (x >= 120 && x <= 200)) {
-        basic.showLeds(`
-            . . # . .
-            . # . # .
-            # # # # #
-            . . # . .
-            . . # . .
-            `)
-        iBIT.Motor(ibitMotor.Forward, 35)
-    } else if (x < 120) {
-        iBIT.Turn(ibitTurn.Right, 30)
-    } else if (x > 200) {
-        iBIT.Turn(ibitTurn.Left, 30)
-    }
-}
-function fc_maintenance () {
-    if (input.buttonIsPressed(Button.A)) {
-        maintenance = maintenance + 1
-        if (maintenance == 1) {
-            basic.showString("CT")
-        } else if (maintenance == 2) {
-            basic.showString("ST")
-        } else if (maintenance >= 3) {
-            maintenance = 0
-            basic.clearScreen()
-        }
-    }
+function fc_maintenance_test () {
     if (maintenance == 1) {
         basic.showString("" + (testcam))
         if (input.buttonIsPressed(Button.B)) {
             testcam = testcam + 1
             if (testcam == 1) {
-                iBIT.Servo(ibitServo.SV2, 5)
+                iBIT.Servo(ibitServo.SV2, 15)
             } else if (testcam == 2) {
                 iBIT.Servo(ibitServo.SV2, 35)
             } else if (testcam == 3) {
@@ -108,11 +71,53 @@ function fc_maintenance () {
             if (testservo == 1) {
                 iBIT.Servo(ibitServo.SV1, 0)
             } else if (testservo == 2) {
-                iBIT.Servo(ibitServo.SV1, 30)
+                iBIT.Servo(ibitServo.SV1, 50)
             } else if (testservo == 3) {
                 iBIT.Servo(ibitServo.SV1, 90)
                 testservo = 0
             }
+        }
+    }
+}
+function objectfollowing () {
+    if (y > 160 && (x >= 120 && x <= 200)) {
+        iBIT.MotorStop()
+        basic.showLeds(`
+            # # # # #
+            # . . . #
+            # . . . #
+            # . . . #
+            # # # # #
+            `)
+        iBIT.Servo(ibitServo.SV1, 50)
+        y = -1
+        step = 1
+    } else if (y < 160 && (x >= 120 && x <= 200)) {
+        basic.showLeds(`
+            . . # . .
+            . # . # .
+            # # # # #
+            . . # . .
+            . . # . .
+            `)
+        iBIT.Motor(ibitMotor.Forward, 30)
+    } else if (x < 120) {
+        iBIT.Turn(ibitTurn.Right, 30)
+    } else if (x > 200) {
+        iBIT.Turn(ibitTurn.Left, 30)
+    }
+}
+function fc_maintenance () {
+    if (input.buttonIsPressed(Button.A)) {
+        maintenance = maintenance + 1
+        iBIT.MotorStop()
+        if (maintenance == 1) {
+            basic.showString("CT")
+        } else if (maintenance == 2) {
+            basic.showString("ST")
+        } else if (maintenance >= 3) {
+            maintenance = 0
+            basic.clearScreen()
         }
     }
 }
@@ -121,8 +126,8 @@ function runtest () {
         iBIT.Servo(ibitServo.SV1, 0)
         iBIT.Servo(ibitServo.SV1, 70)
         basic.pause(300)
-        iBIT.Motor(ibitMotor.Backward, 90)
-        basic.pause(400)
+        iBIT.Motor(ibitMotor.Backward, 100)
+        basic.pause(500)
         iBIT.MotorStop()
         basic.pause(200)
         iBIT.Motor(ibitMotor.Forward, 100)
@@ -132,27 +137,22 @@ function runtest () {
         iBIT.Servo(ibitServo.SV1, 0)
     } else if (input.buttonIsPressed(Button.A)) {
         iBIT.Servo(ibitServo.SV1, 0)
-        iBIT.Motor(ibitMotor.Backward, 90)
-        basic.pause(400)
+        iBIT.Motor(ibitMotor.Backward, 100)
+        basic.pause(700)
         iBIT.MotorStop()
         iBIT.Servo(ibitServo.SV1, 40)
-        basic.pause(200)
-        iBIT.Motor(ibitMotor.Forward, 100)
         basic.pause(500)
+        iBIT.Motor(ibitMotor.Forward, 100)
+        basic.pause(700)
         iBIT.MotorStop()
         basic.pause(300)
         iBIT.Servo(ibitServo.SV1, 0)
     }
 }
 function ready_crashOBJ () {
-    iBIT.Servo(ibitServo.SV1, 90)
+    iBIT.Servo(ibitServo.SV1, 50)
     basic.pause(500)
-    iBIT.Motor(ibitMotor.Forward, 30)
-    basic.pause(1000)
-    iBIT.Servo(ibitServo.SV1, 0)
-    basic.pause(500)
-    iBIT.MotorStop()
-    ball = 1
+    fw_ball()
 }
 function Movement_test () {
     basic.showString("M")
@@ -173,7 +173,18 @@ function nameset () {
     huskylens.initI2c()
     huskylens.initMode(protocolAlgorithm.ALGORITHM_COLOR_RECOGNITION)
     huskylens.writeName(1, "test")
-    huskylens.writeOSD("tikxd", 160, 190)
+}
+function fw_ball () {
+    if (y > 130 && (x >= 120 && x <= 200)) {
+        iBIT.MotorStop()
+        iBIT.Servo(ibitServo.SV1, 0)
+    } else if (y < 130 && (x >= 120 && x <= 200)) {
+        iBIT.Motor(ibitMotor.Forward, 20)
+    } else if (x < 120) {
+        iBIT.Turn(ibitTurn.Right, 20)
+    } else if (x > 200) {
+        iBIT.Turn(ibitTurn.Left, 20)
+    }
 }
 function crashOBJ () {
     basic.showIcon(IconNames.Skull)
@@ -189,7 +200,7 @@ function crashOBJ () {
     ball = 1
 }
 function Huskylens () {
-    huskylens.request()
+    iBIT.Servo(ibitServo.SV2, 35)
     if (huskylens.isAppear(1, HUSKYLENSResultType_t.HUSKYLENSResultBlock)) {
         color = 1
         x = huskylens.readeBox(1, Content1.xCenter)
@@ -205,6 +216,9 @@ function Huskylens () {
         iBIT.MotorStop()
     }
 }
+let delay_one = 0
+let ball = 0
+let step = 0
 let x = 0
 let y = 0
 let S_y = 0
@@ -214,15 +228,39 @@ let testservo = 0
 let maintenance = 0
 let color = 0
 let tag_one = 0
-let ball = 0
-resetServo()
 nameset()
-ball = 0
+huskylens.clearOSD()
+huskylens.writeOSD("tikxd", 160, 200)
 basic.forever(function () {
-    if (ball == 0) {
-        Huskylens()
-        tag_one = 1
-    } else if (ball == 1) {
-        check_shoot()
+    fc_maintenance()
+    serial.writeValue("x", x)
+    serial.writeValue("y", y)
+    if (maintenance > 0) {
+        fc_maintenance_test()
+        huskylens.request()
+        x = huskylens.readeBox(1, Content1.xCenter)
+        y = huskylens.readeBox(1, Content1.yCenter)
+    } else {
+        huskylens.request()
+        x = huskylens.readeBox(color, Content1.xCenter)
+        y = huskylens.readeBox(color, Content1.yCenter)
+        if (step == 0) {
+            Huskylens()
+            tag_one = 1
+            delay_one = 1
+        } else if (step == 1) {
+            if (delay_one == 1) {
+                iBIT.Servo(ibitServo.SV2, 5)
+                basic.pause(1000)
+                y = huskylens.readeBox(color, Content1.yCenter)
+                delay_one = 0
+                y = -1
+            }
+            fw_ball()
+        } else if (step == 2) {
+            check_shoot()
+        } else {
+        	
+        }
     }
 })
